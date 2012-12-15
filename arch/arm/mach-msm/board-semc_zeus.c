c533d49d71be568a450265d5086eb9c0694bf4a0
CosmicDan <cosmicdan@cosmicdan-vm.(none)>
2012-12-15 16:48:01 +1100

    Major multiboot update part 2
    Because the mmc driver isn't loaded until boot init, we need to replicate the post-fs-data commands in the multiboot helper. Hopefully this is good enough to provide a 100% reliable data partition for slots 2 and 3 - if errors are still reported I can at least debug it easier now.

--- arch/arm/mach-msm/board-semc_zeus.c
+++ arch/arm/mach-msm/board-semc_zeus.c
@@ -119,8 +119,6 @@
 #define MSM_FB_SIZE		(864 * 480 * 4 * 2)
 #endif
 
-#define MSM_GPU_PHYS_SIZE       SZ_4M
-
 #define MSM_PMEM_CAMERA_SIZE    0x0000000
 //////////////////////////////////////////////
 // 720p
@@ -3425,14 +3423,6 @@ static void __init fb_size_setup(char **p)
 }
 
 __early_param("fb_size=", fb_size_setup);
-
-static unsigned gpu_phys_size = MSM_GPU_PHYS_SIZE;
-static void __init gpu_phys_size_setup(char **p)
-{
-	gpu_phys_size = memparse(*p, p);
-}
-
-__early_param("gpu_phys_size=", gpu_phys_size_setup);
 
 static unsigned pmem_adsp_size = MSM_PMEM_ADSP_SIZE;
 static void __init pmem_adsp_size_setup(char **p)
