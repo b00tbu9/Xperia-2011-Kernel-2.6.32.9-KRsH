8da25c531cc5e4096c5fdaffa239e86df4f6b9d0
CosmicDan <cosmicdan@cosmicdan-vm.(none)>
2012-12-26 22:16:29 +1100

    Multiboot - repair bugfix
    Pre-Recovery Aroma can not copy the log from tmp to sdcard for some strange reason, so instead we keep it on cache and move it to sdcard the next time the Boot menu is entered.

--- ramdisk/res/aromapreinstall/repair_helper.sh
+++ ramdisk/res/aromapreinstall/repair_helper.sh
@@ -2,11 +2,11 @@
 
 start()
 {
-    echo "#####" > /tmp/turbo_repair.log
-    echo "Turbo Repair started" >> /tmp/turbo_repair.log
-    date >> /tmp/turbo_repair.log
-    echo "#####" >> /tmp/turbo_repair.log
-    echo " " >> /tmp/turbo_repair.log
+    echo "#####" > /cache/turbo_repair.log
+    echo "Turbo Repair started" >> /cache/turbo_repair.log
+    date >> /cache/turbo_repair.log
+    echo "#####" >> /cache/turbo_repair.log
+    echo " " >> /cache/turbo_repair.log
 }
 
 repairsd()
@@ -14,19 +14,19 @@ repairsd()
     umount /sdcard
     umount /sd-ext
     if   [ "$2" == "1" ]; then
-        echo " " >> /tmp/turbo_repair.log
-        echo "### About to run fsck_msdos on microSD FAT32 partition... " >> /tmp/turbo_repair.log
-        echo "###" >> /tmp/turbo_repair.log
-        fsck_msdos -y /dev/block/mmcblk0p1 >> /tmp/turbo_repair.log
+        echo " " >> /cache/turbo_repair.log
+        echo "### About to run fsck_msdos on microSD FAT32 partition... " >> /cache/turbo_repair.log
+        echo "###" >> /cache/turbo_repair.log
+        fsck_msdos -y /dev/block/mmcblk0p1 >> /cache/turbo_repair.log
     elif [ "$2" == "2" ]; then
-        echo " " >> /tmp/turbo_repair.log
+        echo " " >> /cache/turbo_repair.log
         if [ -e /dev/block/mmcblk0p2 ]; then
-            echo "### About to run e2fsck on microSD sd-ext partition... " >> /tmp/turbo_repair.log
-            echo "###" >> /tmp/turbo_repair.log
-            e2fsck -p -f -v /dev/block/mmcblk0p2 >> /tmp/turbo_repair.log
+            echo "### About to run e2fsck on microSD sd-ext partition... " >> /cache/turbo_repair.log
+            echo "###" >> /cache/turbo_repair.log
+            e2fsck -p -f -v /dev/block/mmcblk0p2 >> /cache/turbo_repair.log
         else
-            echo "### Skipped e2fsck on microSD sd-ext partition (not found) " >> /tmp/turbo_repair.log
-            echo "###" >> /tmp/turbo_repair.log
+            echo "### Skipped e2fsck on microSD sd-ext partition (not found) " >> /cache/turbo_repair.log
+            echo "###" >> /cache/turbo_repair.log
         fi
     fi
 }
@@ -36,21 +36,21 @@ fixpermissions()
     umount /sdcard
     mount -o rw /dev/block/mmcblk0p1 /sdcard
     umount /data
-    echo " " >> /tmp/turbo_repair.log
+    echo " " >> /cache/turbo_repair.log
     if [ "$2" == "1" ]; then
-        echo "### About to repair permissions for Internal system... " >> /tmp/turbo_repair.log
-        echo "###" >> /tmp/turbo_repair.log
-        mount -o rw /dev/block/mtdblock1 /data >> /tmp/turbo_repair.log
-        /sbin/fix_permissions >> /tmp/turbo_repair.log
+        echo "### About to repair permissions for Internal system... " >> /cache/turbo_repair.log
+        echo "###" >> /cache/turbo_repair.log
+        mount -o rw /dev/block/mtdblock1 /data >> /cache/turbo_repair.log
+        /sbin/fix_permissions >> /cache/turbo_repair.log
     else
         if [ -e /sdcard/userdata$2.ext2.img ]; then
-            echo "### About to repair permissions for Slot $2... " >> /tmp/turbo_repair.log
-            echo "###" >> /tmp/turbo_repair.log
-            mount -t ext2   -o rw,loop,noatime,nosuid,nodev  /sdcard/userdata$2.ext2.img  /data >> /tmp/turbo_repair.log
-            /sbin/fix_permissions >> /tmp/turbo_repair.log
+            echo "### About to repair permissions for Slot $2... " >> /cache/turbo_repair.log
+            echo "###" >> /cache/turbo_repair.log
+            mount -t ext2   -o rw,loop,noatime,nosuid,nodev  /sdcard/userdata$2.ext2.img  /data >> /cache/turbo_repair.log
+            /sbin/fix_permissions >> /cache/turbo_repair.log
         else
-            echo "### Slot $2 has no userdata image, fix permissions skipped. " >> /tmp/turbo_repair.log
-            echo "###" >> /tmp/turbo_repair.log
+            echo "### Slot $2 has no userdata image, fix permissions skipped. " >> /cache/turbo_repair.log
+            echo "###" >> /cache/turbo_repair.log
         fi
     fi
     umount /data
@@ -62,32 +62,29 @@ repairslot()
     mount -o rw /dev/block/mmcblk0p1 /sdcard
     umount /system
     umount /data
-    echo " " >> /tmp/turbo_repair.log
+    echo " " >> /cache/turbo_repair.log
     if [ -e /sdcard/$3$2.ext2.img ]; then
-        echo "### About to run e2fsck on $3 partition for Slot $2... " >> /tmp/turbo_repair.log
-        echo "###" >> /tmp/turbo_repair.log
-        e2fsck -p -f -v /sdcard/$3$2.ext2.img >> /tmp/turbo_repair.log
+        echo "### About to run e2fsck on $3 partition for Slot $2... " >> /cache/turbo_repair.log
+        echo "###" >> /cache/turbo_repair.log
+        e2fsck -p -f -v /sdcard/$3$2.ext2.img >> /cache/turbo_repair.log
     else
-        echo "### Slot $2 has no $3 image, e2fsck skipped. " >> /tmp/turbo_repair.log
-        echo "###" >> /tmp/turbo_repair.log
+        echo "### Slot $2 has no $3 image, e2fsck skipped. " >> /cache/turbo_repair.log
+        echo "###" >> /cache/turbo_repair.log
     fi
 }
 
 finish()
 {
-    echo " " >> /tmp/turbo_repair.log
-    echo "#####" >> /tmp/turbo_repair.log
-    echo "Turbo Repair finished" >> /tmp/turbo_repair.log
-    date >> /tmp/turbo_repair.log
-    echo "#####" >> /tmp/turbo_repair.log
+    echo " " >> /cache/turbo_repair.log
+    echo "#####" >> /cache/turbo_repair.log
+    echo "Turbo Repair finished" >> /cache/turbo_repair.log
+    date >> /cache/turbo_repair.log
+    echo "#####" >> /cache/turbo_repair.log
 }
 
 copylog()
 {
-    umount /sdcard
-    mount -o rw /dev/block/mmcblk0p1 /sdcard
-    cp /tmp/turbo_repair.log /sdcard/turbo_repair.log
-    rm /tmp/turbo_repair.log
+    touch /cache/turbo_repair.ready2copy
 }
 
 $1 $1 $2 $3
